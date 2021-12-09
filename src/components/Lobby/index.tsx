c166728ac52a239d641f422b42e84875f14b70cf
JLentremy <jeremy@yada.email>
2021-12-09 14:36:10 +0100

    opti (+lisibilité dans Lobby)

--- src/components/Lobby/index.tsx
+++ src/components/Lobby/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useAppContext } from "../../app/AppContext";
 import { rollADie } from "../../app/utils";
+import { StatProps } from "../Character/types";
 import FighterItem from "./FighterItem";
 import Modal from "./Modal";
 import { DefendingProps, FightProps, TakeDamageProps } from "./types";
@@ -9,42 +10,43 @@ const Lobby = ({ attacker, opponent }: FightProps) => {
   const context = useAppContext();
   const [texts, setTexts] = useState<string[]>([]);
   const [round, setRound] = useState(1);
-  const [end, setEnd] = useState(false);
+  const [endgame, setEndgame] = useState(false);
   const alert = (text: string) => {
     setTexts([...texts, text]);
   };
+
   const attacking = ({ attacker, opponent }: FightProps) => {
     const nbFace = attacker.attack.value;
     const atk = rollADie(nbFace);
+    const info = `- ${attacker.name} attaque ${opponent.name} (1D${nbFace}: ${atk}).`;
 
-    alert(`- ${attacker.name} attaque ${opponent.name} (1D${nbFace}: ${atk}).`);
-    console.log(
-      `- ${attacker.name} attaque ${opponent.name} (1D${nbFace}: ${atk}).`
-    );
+    alert(info);
+    console.log(info);
 
     return atk;
   };
+
   const defending = ({ opponent, atk }: DefendingProps) => {
     const def = opponent.defense.value;
+    const info = `- ${opponent.name} se défend (dmg:${atk} - def:${def}).`;
 
-    alert(`- ${opponent.name} se défend (dmg:${atk} - def:${def}).`);
-    console.log(`- ${opponent.name} se défend (dmg:${atk} - def:${def}).`);
+    alert(info);
+    console.log(info);
 
     return Math.max(0, atk - def);
   };
+
   const takeDamage = ({ attacker, opponent, damage }: TakeDamageProps) => {
+    const info = `- Mais ${opponent.name} est blessé ! (-${damage} health).`;
+    const infoMag = `- La magie de ${attacker.name} affecte ${opponent.name} ! (-${damage} health).`;
     const mag = attacker.magik.value;
 
-    alert(`- Mais ${opponent.name} est blessé ! (-${damage} health).`);
-    console.log(`- Mais ${opponent.name} est blessé ! (-${damage} health).`);
+    alert(info);
+    console.log(info);
 
     if (damage === mag) {
-      alert(
-        `- La magie de ${attacker.name} affecte ${opponent.name} ! (-${damage} health).`
-      );
-      console.log(
-        `- La magie de ${attacker.name} affecte ${opponent.name} ! (-${damage} health).`
-      );
+      alert(infoMag);
+      console.log(infoMag);
       damage = damage * 2;
     }
 
@@ -56,96 +58,75 @@ const Lobby = ({ attacker, opponent }: FightProps) => {
       },
     };
   };
-  const isDefeated = (opponent: FightProps["opponent"]) => {
+
+  const checkEndgame = ({ attacker, opponent }: FightProps) => {
+    const info = `${opponent.name} est vaincu !`;
     const defeated = opponent.health.value === 0;
+    const healing = (health: StatProps) => {
+      return {
+        ...health,
+        value: health.max_value ? health.max_value : health.value,
+      };
+    };
 
     if (defeated) {
-      alert(`${opponent.name} est vaincu !`);
-      console.log(`${opponent.name} est vaincu !`);
-    }
+      alert(info);
+      console.log(info);
 
-    return defeated;
+      context.updateCharacter({
+        ...attacker,
+        rank: attacker.rank + 1,
+        skill_pts: attacker.skill_pts + 1,
+        health: healing(attacker.health),
+      });
+      context.updateCharacter({
+        ...opponent,
+        rank: Math.max(1, opponent.rank - 1),
+        health: healing(opponent.health),
+      });
+      setEndgame(true);
+    }
   };
-  const runTurn = ({ attacker, opponent }: FightProps) => {
+
+  const runTurn = ({
+    attacker,
+    opponent,
+    strikeback,
+  }: FightProps & { strikeback?: Boolean }) => {
+    const info = `Tour de ${attacker.name} :`;
     let atk;
     let damage;
+
+    alert(info);
+    console.log(info);
 
     atk = attacking({ attacker, opponent });
     damage = defending({ opponent, atk });
 
     if (damage > 0) {
-      return takeDamage({ attacker, opponent, damage });
+      let newOpponent = takeDamage({ attacker, opponent, damage });
+      strikeback
+        ? context.setAttacker(newOpponent)
+        : context.setOpponent(newOpponent);
+      checkEndgame({ attacker, opponent: newOpponent });
     }
   };
+
   const runRound = () => {
-    let newOpponent;
-    let newAttacker;
+    const info = `Round ${round} :`;
 
-    if (context.opponent === null || context.attacker === null) return;
+    alert(info);
+    console.log(info);
 
-    alert(`Round ${round} :`);
-    console.log(`Round ${round} :`);
-
-    alert(`Tour de ${attacker.name} :`);
-    console.log(`Tour de ${attacker.name} :`);
-
-    newOpponent = runTurn({ attacker, opponent });
-    if (newOpponent) context.setOpponent(newOpponent);
-    if (newOpponent && isDefeated(newOpponent)) {
-      context.updateCharacter({
-        ...attacker,
-        rank: attacker.rank + 1,
-        skill_pts: attacker.skill_pts + 1,
-        health: {
-          ...attacker.health,
-          value: attacker.health.max_value
-            ? attacker.health.max_value
-            : attacker.health.value,
-        },
+    runTurn({ attacker, opponent });
+    if (!endgame)
+      runTurn({
+        attacker: opponent,
+        opponent: attacker,
+        strikeback: true,
       });
-      context.updateCharacter({
-        ...newOpponent,
-        rank: Math.max(1, newOpponent.rank - 1),
-        health: {
-          ...newOpponent.health,
-          value: newOpponent.health.max_value
-            ? newOpponent.health.max_value
-            : newOpponent.health.value,
-        },
-      });
-      setEnd(true);
-    } else {
-      alert(`Tour de ${opponent.name} :`);
-      console.log(`Tour de ${opponent.name} :`);
 
-      newAttacker = runTurn({ attacker: context.opponent, opponent: attacker });
-      if (newAttacker) context.setAttacker(newAttacker);
-      if (newAttacker && isDefeated(newAttacker)) {
-        context.updateCharacter({
-          ...newAttacker,
-          rank: Math.max(1, newAttacker.rank - 1),
-          health: {
-            ...newAttacker.health,
-            value: newAttacker.health.max_value
-              ? newAttacker.health.max_value
-              : newAttacker.health.value,
-          },
-        });
-        context.updateCharacter({
-          ...opponent,
-          rank: opponent.rank + 1,
-          skill_pts: opponent.skill_pts + 1,
-          health: {
-            ...opponent.health,
-            value: opponent.health.max_value
-              ? opponent.health.max_value
-              : opponent.health.value,
-          },
-        });
-        setEnd(true);
-      }
-    }
-
+    console.log(texts);
     setRound(round + 1);
   };
 
@@ -156,7 +137,7 @@ const Lobby = ({ attacker, opponent }: FightProps) => {
         <FighterItem fighter={opponent} />
       </div>
       <Modal texts={texts} />
-      {end ? (
+      {endgame ? (
         <button
           onClick={() => {
             context.setAttacker(null);
