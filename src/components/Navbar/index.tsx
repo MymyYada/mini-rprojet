3d619e434489006ae742a2a0c38122a9e6ce1670
JLentremy <jeremy@yada.email>
2021-12-10 23:08:23 +0100

    naming

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -32,13 +32,13 @@ const Navbar = () => {
               href="#"
               className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
             >
-              Sign in
+              Connexion
             </a>
             <a
               href="#"
               className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
             >
-              Sign up
+              Inscription
             </a>
           </div>
         </div>
